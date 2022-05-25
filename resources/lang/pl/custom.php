6175f8ec1dd2f65f2c801066c34711775b1e10f5
Seweryn Drag <dragseweryn@zohomail.eu>
2022-05-25 19:48:30 +0200

    404

--- resources/lang/pl/custom.php
+++ resources/lang/pl/custom.php
@@ -10,4 +10,5 @@ return [
 
     'not_allowed' => 'Nie masz uprawnień do wykonania tej akcji.',
     'not_reserved' => 'Nieudana rezerwacja lotu.',
+    '404_notfound' => 'Nie znaleziono strony o tym adresie. Za kilka sekund nastąpi automatyczne przekierowanie do strony głównej',
 ];
